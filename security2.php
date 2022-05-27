58d9e35136d6ea8683da7169f76f1ff796d088a5
Fernando Cruz <86312129+Nandotecno@users.noreply.github.com>
2022-05-27 18:33:04 -0300

    Add files via upload

---
+++ security2.php
@@ -0,0 +1,19 @@
+<?php
+
+$bds = "theunion1000.com.br";
+
+if($bds == "theunion1000.com.br"){
+    $bd = 'mtmse924_saefer';
+    $bd_user = 'mtmse924_view-sf';
+    $bd_password = '*_DjUrW9?YuO';
+    $local = 'theunion1000.com.br';
+}else{
+    $bd = 'saeferre_artigos';
+    $bd_user = 'saeferre_view_sf';
+    $bd_password = '$K#@ZklNJNiF';
+    $local = 'saeferrerira.adv.br';
+}
+
+
+
+?>
